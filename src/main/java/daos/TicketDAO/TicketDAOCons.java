873feed17713e9900a606579eede423db61d699b
Pham Tung Anh <toners117@gmail.com>
2022-04-22 15:50:19 +0700

    Update TicketDAOCons.java

--- src/main/java/daos/TicketDAO/TicketDAOCons.java
+++ src/main/java/daos/TicketDAO/TicketDAOCons.java
@@ -6,7 +6,7 @@ public class TicketDAOCons {
     public static final String TICKET_QUERY_INSERT = "INSERT INTO ticket(bookingTime,customerName, licensePlate,tripId) VALUES (?,?,?,?)";
     public static final String TICKET_QUERY_UPDATE = "UPDATE ticket set bookingTime = ?,customerName = ?, licensePlate = ?,tripId = ? where ticketId = ?";
     public static final String TICKET_QUERY_REMOVE_BY_TICKETID = "DELETE FROM ticket WHERE ticketId = ?";
-    public static final String TICKET_QUERY_GET_ALL_LICENSE_PLATE = ""
+    public static final String TICKET_QUERY_GET_ALL_LICENSE_PLATE = "";
     public static final String SUCCESS = "success";
     public static final String FAIL = "failed";
 }
